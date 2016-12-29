d20ddebfaa5e145457aa3fd8f5dc05aa3b7eb81b
HarriA <harrialasi@gmail.com>
2016-12-29 18:54:22 +0200

    Enable rotating thew object on touch devices
    Change-Id: I71d5db57586818e46a0b79f9a2f50832aa338765

--- modules/stlviewer.js
+++ modules/stlviewer.js
@@ -1,4 +1,8 @@
-  	  var rotateStartPoint = new THREE.Vector3(0, 0, 1);
+	  document.addEventListener("touchstart", touchHandler, true);
+      document.addEventListener("touchmove", touchHandler, true);
+      document.addEventListener("touchend", touchHandler, true);
+      document.addEventListener("touchcancel", touchHandler, true); 
+	  var rotateStartPoint = new THREE.Vector3(0, 0, 1);
   	  var rotateEndPoint = new THREE.Vector3(0, 0, 1);
   	  var deltaX = 0,
   	    deltaY = 0;
@@ -180,7 +184,29 @@
   	    return;
   	  }
 
+	function touchHandler(event)
+{
+    var touches = event.changedTouches,
+        first = touches[0],
+        type = "";
+    switch(event.type)
+    {
+        case "touchstart": type = "mousedown"; break;
+        case "touchmove":  type = "mousemove"; break;        
+        case "touchend":   type = "mouseup";   break;
+        default:           return;
+    }
 
+    var simulatedEvent = document.createEvent("MouseEvent");
+    simulatedEvent.initMouseEvent(type, true, true, window, 1, 
+                                  first.screenX, first.screenY, 
+                                  first.clientX, first.clientY, false, 
+                                  false, false, false, 0/*left*/, null);
+
+    first.target.dispatchEvent(simulatedEvent);
+    event.preventDefault();
+}
+ 
   	  function onDocumentMouseDown(event) {
 
   	    if (event.button != 0) return;
